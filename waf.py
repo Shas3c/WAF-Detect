533a24ae806e9f0b338b6289575003e2a2502662
Sahil Sharma <83185100+Shas3c@users.noreply.github.com>
2023-07-09 00:58:18 +0530

    Update waf.py

--- waf.py
+++ waf.py
@@ -9,7 +9,6 @@ import threading
 from colorama import init, Fore, Style
 import datetime
 
-#f = os.system('nuclei -silent -nc -target https://bittorrent.com -t technologies/waf-detect.yaml -tags misc')
 
 def banner():
     init()
@@ -20,41 +19,39 @@ def banner():
     {Fore.BLUE}		 W W W W  A    A  F {Style.RESET_ALL}
     {Fore.BLUE}  		  W   W   A    A  F {Style.RESET_ALL}
       
-    {Fore.CYAN} 		      			--- Sahil Sharma (CASB) {Style.RESET_ALL}
+    {Fore.CYAN} 		      			--- Sahil Sharma (Shas3c) {Style.RESET_ALL}
     """
     print(banner)
 
 def waf(url, count):
-    #url = 'bittorrent.com'
-    #count = int()
-    time = (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
 
     command = f"nuclei -silent -nc -target https://{url} -t technologies/waf-detect.yaml -tags misc"
 
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
     output = result.stdout.strip()
-    error = result.stderr.strip()
-
-    #print(output)
+    
     match = re.search(r'waf-detect:(.*?)\]', output)
-#print(error)
+
     if match:
         value=match.group(1)
-        #print(value)
-        print(f"{url} ---> {Fore.GREEN}{Style.BRIGHT}{value}{Style.RESET_ALL} | {Fore.YELLOW}#{count}{Style.RESET_ALL}  {time}")
-        data = [url, 'True', value, (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S")]
+        
+        print(f"{url} ---> {Fore.GREEN}{Style.BRIGHT}{value}{Style.RESET_ALL} | {Fore.YELLOW}#{count}{Style.RESET_ALL}")
+        data = [url, 'True', value]
     else:
         print(f"{url} ---> {Fore.RED}No WAF{Style.RESET_ALL} | {Fore.YELLOW}#{count}{Style.RESET_ALL}  {time}")
-        data = [url, 'False', '', (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S")]
+        data = [url, 'False', '']
     return data
     
 if __name__=='__main__':
     banner()
-    Threads = 24
-    #count=1
+    Threads = 4                        # Change threads according to your system requirements
+
+    input_path = 'url.txt'             # pass the full path of your input file
+    output_path = 'waf-results.csv'    #pass the full path of output file generation
+    
     url_list=[]
-    with open('/root/Desktop/url.txt','r') as fileobject:
+    with open(input_path,'r') as fileobject:
         for url in fileobject:
             url=url.strip()
             url_list.append(url)
@@ -62,9 +59,9 @@ if __name__=='__main__':
     
     list_len = len(url_list)
     
-    with open('/root/Desktop/waf-results2.csv','w',newline='') as f:
+    with open(output_path,'w',newline='') as f:
         writer = csv.writer(f, delimiter=',')
-        header = ['Domain','Firewall','Manufacturer', (datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S")]
+        header = ['Domain','Firewall','Manufacturer']
         writer.writerow(header)
         
         with ThreadPoolExecutor(max_workers=min(Threads, list_len)) as tp:
